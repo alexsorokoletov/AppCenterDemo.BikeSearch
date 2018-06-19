4b88b82023cc33f8dfbc3b98050cbd4f6fbfb6ab
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2018-06-19 19:05:30 -0400

    AppCenter integrated

--- BikeSearch/App.xaml.cs
+++ BikeSearch/App.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +18,7 @@ namespace BikeSearch
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("ios=93dd2600-deae-4958-98fc-61e04a13c4a4;" + "uwp={Your UWP App secret here};" + "android={Your Android App secret here}", typeof(Analytics), typeof(Crashes));
         }
     }
 }
